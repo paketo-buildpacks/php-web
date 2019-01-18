28ed0c778be67e145b72e49c8b100f482162ded4
Daniel Mikusa <dmikusa@pivotal.io>
2019-01-17 21:35:05 -0500

    Added a web & script dependency

--- phpapp/phpapp.go
+++ phpapp/phpapp.go
@@ -1,3 +1,7 @@
 package phpapp
 
-const Dependency = "php-app"
+// WebDependency in the buildplan indiates that this is a web app
+const WebDependency = "php-web"
+
+// ScriptDependency in the buildplan indicates that this is a script app
+const ScriptDependency = "php-script"
