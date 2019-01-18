26e814969e4dac3e97551106b49cba342b29fdb8
Daniel Mikusa <dmikusa@pivotal.io>
2019-01-17 23:06:44 -0500

    Reorganized consts & added consts for supported web server types.

--- phpapp/phpapp.go
+++ phpapp/phpapp.go
@@ -1,7 +1,18 @@
 package phpapp
 
-// WebDependency in the buildplan indiates that this is a web app
-const WebDependency = "php-web"
+const (
+	// WebDependency in the buildplan indiates that this is a web app
+	WebDependency = "php-web"
 
-// ScriptDependency in the buildplan indicates that this is a script app
-const ScriptDependency = "php-script"
+	// ScriptDependency in the buildplan indicates that this is a script app
+	ScriptDependency = "php-script"
+
+	// Nginx is text user can specify to request Nginx Web Server
+	Nginx = "nginx"
+
+	// ApacheHttpd is text user can specify to request Apache Web Server
+	ApacheHttpd = "httpd"
+
+	// PhpWebServer is text user can specify to use PHP's built-in Web Server
+	PhpWebServer = "php-server"
+)
